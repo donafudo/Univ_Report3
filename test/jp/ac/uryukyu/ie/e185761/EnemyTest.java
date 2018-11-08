3c44937e69019a15450aa3f791a3588adfba8134
tawatamasato <donafudo@gmail.com>
2018-11-08 17:44:26 +0900

    Added enemytest

---
+++ test/jp/ac/uryukyu/ie/e185761/EnemyTest.java
@@ -0,0 +1,22 @@
+package jp.ac.uryukyu.ie.e185761;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void attack() {
+        Enemy enemy = new Enemy("スライム",10,10);
+        Hero hero=new Hero("tanaka",10,4);
+        int hp=hero.hitPoint;
+
+        enemy.hitPoint=0;
+        for(int i=0;i<5;i++){
+            enemy.attack(hero);
+        }
+
+        assertEquals(hp,hero.hitPoint);
+    }
+}
